19fd3402172e7b4bdcfa4ff67dec98935da4bac4
unknown <robert.kami88@gmail.com>
2020-12-06 07:45:45 +0100

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,17 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    keep_classnames: true,
+                    keep_fnames: true
+                }
+            })
+        ]
+    },
 };
